70cf2ee8cfa385e0164b5b43e3d93fe3b8d24837
kushagra977 <kushagragautam0706@gmail.com>
2019-09-13 10:38:05 +0530

    ALL TEST CASES PASSED INCLUDES ZERO ADDITION AND SIMPLE ADDITION

--- TddCalApp.Test/UnitTest1.cs
+++ TddCalApp.Test/UnitTest1.cs
@@ -18,6 +18,15 @@ namespace TddCalApp.Test
             Assert.AreEqual(5, result, "testing two integer");
 
         }
+        public void ShouldReturnZeroOnPassingPairOfPositiveAndNegativeInteger()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = -3;
+            int num2 = 3;
+            int result;
+            result = testCalculator.Add(num1, num2);
+            Assert.AreEqual(0, result, "Testing pair of integer positive and negative");
+        }
         
     }
 }
